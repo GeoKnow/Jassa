0fefb9d40dd8b4b5e10ddd9db2a84d472f05c22b
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-14 00:40:40 +0200

    fix

--- jassa-js/src/main/webapp/resources/js/additions-to-jassa-core.js
+++ jassa-js/src/main/webapp/resources/js/additions-to-jassa-core.js
@@ -292,7 +292,7 @@
 
 	        var countConcept;
             if(this.isLeftJoin) {
-                var query = facete.ConceptUtils.createAttrQuery(this.attrQuery, this.attrVar, this.isLeftJoin, filterConcept, limit, offset);
+                var query = facete.ConceptUtils.createAttrQuery(this.attrQuery, this.attrVar, this.isLeftJoin, filterConcept, itemLimit, offset);
 
                 countConcept = new facete.Concept(query.getQueryPattern(), this.attrVar);
             } else {
